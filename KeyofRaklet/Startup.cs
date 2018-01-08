e67d265e4674654b237115f142a511c3b23120cb
Msalpdogan <msalpdogan@gmail.com>
2018-01-09 00:25:30 +0300

    Add project files.

---
+++ KeyofRaklet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KeyofRaklet.Startup))]
+namespace KeyofRaklet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
